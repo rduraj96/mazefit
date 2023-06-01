4cf39fe6eb1dab311836ea4ae91fd0daea3fae78
rduraj96 <42787617+rduraj96@users.noreply.github.com>
2023-05-31 20:31:36 -0400

    added weight chart

--- app/(home)/WeightChart.tsx
+++ app/(home)/WeightChart.tsx
@@ -7,37 +7,82 @@ import {
   CartesianGrid,
   Tooltip,
   ResponsiveContainer,
+  LineChart,
+  Line,
 } from "recharts";
 
-type Props = {
-  data: Array<{
-    name: string;
-    protein: number;
-    calories: number;
-    amt: number;
-  }>;
-};
+type Props = {};
 
-const WeightChart = ({ data }: Props) => {
+const WeightChart = (props: Props) => {
+  const data = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+
   return (
     <ResponsiveContainer width="100%" height="100%">
-      <AreaChart
-        width={500}
-        height={400}
+      <LineChart
+        width={300}
+        height={100}
         data={data}
         margin={{
-          top: 10,
-          right: 30,
-          left: 0,
-          bottom: 0,
+          top: 5,
+          right: 4,
+          left: -25,
+          bottom: -10,
         }}
       >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
+        <Line type="natural" dataKey="pv" stroke="#8884d8" strokeWidth={2} />
+        <XAxis
+          dataKey="name"
+          style={{ fontSize: "10px" }}
+          axisLine={false}
+          tickLine={false}
+          // tick={false}
+        />
+        <YAxis style={{ fontSize: "10px" }} axisLine={false} tickLine={false} />
         <Tooltip />
-        <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
-      </AreaChart>
+      </LineChart>
     </ResponsiveContainer>
   );
 };
