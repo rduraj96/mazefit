d9692976026089be764a4c92257033e34aaaa973
rduraj96 <42787617+rduraj96@users.noreply.github.com>
2023-05-31 20:47:20 -0400

    small bug fix

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -17,7 +17,9 @@ const DialogPortal = ({
   ...props
 }: DialogPrimitive.DialogPortalProps) => (
   <DialogPrimitive.Portal className={cn(className)} {...props}>
-    <div className="inset-0 z-50 sm:items-center">{children}</div>
+    <div className="fixed inset-0 z-50 flex items-start justify-center sm:items-center">
+      {children}
+    </div>
   </DialogPrimitive.Portal>
 );
 DialogPortal.displayName = DialogPrimitive.Portal.displayName;
