3a4c09ccc74105e2dde1956ca1a0b02805d528ee
rduraj96 <42787617+rduraj96@users.noreply.github.com>
2023-06-12 17:01:22 -0400

    added spinner component

--- app/(home)/AddMealSearch.tsx
+++ app/(home)/AddMealSearch.tsx
@@ -4,6 +4,7 @@ import { AiOutlineSearch } from "react-icons/ai";
 import React, { Dispatch, useEffect, useState } from "react";
 import SearchFoodCard from "../(shared)/SearchFoodCard";
 import { FoodItem } from "../types";
+import Spinner from "@/components/Spinner";
 
 type Props = {
   setName: Dispatch<React.SetStateAction<string>>;
@@ -11,6 +12,7 @@ type Props = {
   setProtein: Dispatch<React.SetStateAction<string>>;
   setCarbs: Dispatch<React.SetStateAction<string>>;
   setFat: Dispatch<React.SetStateAction<string>>;
+  setCollapsed: Dispatch<React.SetStateAction<boolean>>;
 };
 
 const AddMealSearch = ({
@@ -19,40 +21,57 @@ const AddMealSearch = ({
   setProtein,
   setCarbs,
   setFat,
+  setCollapsed,
 }: Props) => {
   const [searchFood, setSearchFood] = useState("");
   const [foodList, setFoodList] = useState<Array<FoodItem>>([]);
   const [nextPage, setNextPage] = useState<string | null>("");
+  const [searching, setSearching] = useState(false);
+  const [searchClicked, setSearchClicked] = useState(false);
+
+  useEffect(() => {
+    setFoodList([]);
+    setSearchClicked(false);
+    setSearching(false);
+    setSearchFood("");
+  }, []);
 
   const handleSearch = async (viewMore?: string | null) => {
     if (!viewMore) {
       setFoodList([]);
     }
+    setSearchClicked(true);
     try {
+      setSearching(true);
       const response = await fetch(
-        `/api/search?food=${searchFood}${viewMore ? "&" + viewMore : ""}`,
+        `/api/search?food=${searchFood}${
+          viewMore ? "&session=" + viewMore : ""
+        }`,
         {
           method: "GET",
         }
       );
-      const data = await response.json();
-      const params = new URL(data._links.next.href).searchParams;
-      const session = params.get("session");
-      console.log(session);
-      setNextPage(session);
-      data.hints.forEach((item: any) => {
-        setFoodList((foodList) => [
-          ...foodList,
-          {
-            brand: item.food.brand,
-            name: item.food.label,
-            calories: Math.round(item.food.nutrients.ENERC_KCAL),
-            protein: Math.round(item.food.nutrients.PROCNT),
-            carbs: Math.round(item.food.nutrients.CHOCDF),
-            fat: Math.round(item.food.nutrients.FAT),
-          },
-        ]);
-      });
+      if (response.ok) {
+        const data = await response.json();
+        const params = new URL(data._links.next.href).searchParams;
+        const session = params.get("session");
+        console.log(session);
+        setNextPage(session);
+        data.hints.forEach((item: any) => {
+          setFoodList((foodList) => [
+            ...foodList,
+            {
+              brand: item.food.brand,
+              name: item.food.label,
+              calories: Math.round(item.food.nutrients.ENERC_KCAL),
+              protein: Math.round(item.food.nutrients.PROCNT),
+              carbs: Math.round(item.food.nutrients.CHOCDF),
+              fat: Math.round(item.food.nutrients.FAT),
+            },
+          ]);
+        });
+        setSearching(false);
+      }
     } catch (error) {
       console.log(error);
     }
@@ -83,8 +102,22 @@ const AddMealSearch = ({
           <AiOutlineSearch className="text-black" size={20} />
         </div>
       </div>
-      <p className="text-md text-black font-bold mt-4">Search Results...</p>
-      <ScrollArea className="h-96 mt-2 p-2.5">
+      {searchClicked && (
+        <div className="w-full h-10 pt-2">
+          <div className="flex items-center h-full">
+            {searching ? (
+              <div className="flex items-center gap-3">
+                <p className="text-md text-black font-bold">Searching</p>
+                <Spinner />
+              </div>
+            ) : (
+              <div className="text-md text-black font-bold">Search Results</div>
+            )}
+          </div>
+        </div>
+      )}
+
+      <ScrollArea className="mt-2 pr-3 h-96">
         {foodList.map((food, index) => (
           <div key={index} onClick={() => handleSet(food)}>
             <SearchFoodCard food={food} />
