60e2a7e83f28aca83acb76fc0747626c056c0532
rduraj96 <42787617+rduraj96@users.noreply.github.com>
2023-06-13 18:08:31 -0400

    small ui fix

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -82,10 +82,7 @@ const DialogFooter = ({
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
-    className={cn(
-      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
-      className
-    )}
+    className={cn("flex sm:flex-row sm:justify-end sm:space-x-2", className)}
     {...props}
   />
 );
