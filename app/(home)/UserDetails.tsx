338e7ebe31f3ac84592045cabf236dfa9969e60d
rduraj96 <durajrei@gmail.com>
2023-06-26 13:40:32 -0400

    Added overall progress

--- app/(home)/UserDetails.tsx
+++ app/(home)/UserDetails.tsx
@@ -1,87 +1,114 @@
 "use client";
 
 import React, { useState } from "react";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { BsPlus, BsThreeDots } from "react-icons/bs";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useSession } from "next-auth/react";
-import { Button } from "@/components/ui/button";
 import { useGlobalContext } from "../Context/store";
+import { Triangle } from "lucide-react";
 
 type Props = {};
 
 const UserDetails = (props: Props) => {
   const { data: session } = useSession();
   const { userDetails } = useGlobalContext();
+  const [color, setColor] = useState("");
+
+  function hexToHSL(H: string) {
+    // Convert hex to RGB first
+    let r = "",
+      g = "",
+      b = "";
+    if (H.length == 4) {
+      r = "0x" + H[1] + H[1];
+      g = "0x" + H[2] + H[2];
+      b = "0x" + H[3] + H[3];
+    } else if (H.length == 7) {
+      r = "0x" + H[1] + H[2];
+      g = "0x" + H[3] + H[4];
+      b = "0x" + H[5] + H[6];
+    }
+    // Then to HSL
+    let r1 = Number(r);
+    r1 /= 255;
+    let g1 = Number(g);
+    g1 /= 255;
+    let b1 = Number(b);
+    b1 /= 255;
+    let cmin = Math.min(r1, g1, b1),
+      cmax = Math.max(r1, g1, b1),
+      delta = cmax - cmin,
+      h = 0,
+      s = 0,
+      l = 0;
+
+    if (delta == 0) h = 0;
+    else if (cmax == r1) h = ((g1 - b1) / delta) % 6;
+    else if (cmax == g1) h = (b1 - r1) / delta + 2;
+    else h = (r1 - g1) / delta + 4;
+
+    h = Math.round(h * 60);
+
+    if (h < 0) h += 360;
+
+    l = (cmax + cmin) / 2;
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+
+    return "hsl(" + h + "," + s + "%," + l + "%)";
+  }
+
+  const changeBackgroundColor = (hex: string) => {
+    let newColor = hexToHSL(hex);
+    console.log(newColor);
+    document.documentElement.style.setProperty("--accent", newColor);
+  };
 
   return (
-    <section className="h-full w-full pt-7">
-      <div className="bg-foreground h-full shadow-md px-7 py-5 rounded-xl">
-        {/* <div className="flex justify-between items-center">
-          <p className="text-black text-lg font-bold">Profile</p>
-          <div>
-            <Dialog>
-              <DialogTrigger>
-                <div className="h-10 w-10 flex justify-center items-center cursor-pointer">
-                  <BsThreeDots size={20} className="text-black" />
-                </div>
-              </DialogTrigger>
-              <DialogContent>
-                <DialogHeader>
-                  <DialogTitle>Why is this not rendering?</DialogTitle>
-                  <DialogDescription>
-                    This action cannot be undone. This will permanently delete
-                    your account and remove your data from our servers.
-                  </DialogDescription>
-                </DialogHeader>
-              </DialogContent>
-            </Dialog>
-          </div>
-        </div>
-        <div className="mt-10">
-          <div className="flex justify-center items-center">
-            <Avatar className="rounded-full aspect-auto h-20 w-20">
-              <AvatarImage src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80" />
-              <AvatarFallback>
-                {session?.user?.name?.substring(0, 2).toUpperCase()}
-              </AvatarFallback>
-            </Avatar>
-          </div>
-          <div className="text-black text-xl font-bold flex items-center justify-center mt-5">
-            {session?.user?.name}
-          </div>
-          <div className="mt-8 flex text-sm justify-around text-neutral-600 py-3 rounded-xl bg-[#a8bbd1] shadow-md">
-            <div className="flex flex-col items-center justify-center gap-1">
-              <p className="font-semibold text-lg text-gray-100">
-                {userDetails.weight} lb
-              </p>
-              <p>Weight</p>
-            </div>
-            <div className="flex flex-col items-center justify-center gap-1">
-              <p className="font-semibold text-lg text-gray-100">
-                {Math.floor(userDetails.height / 12)}ft{" "}
-                {userDetails.height % 12}in
-              </p>
-              <p>Height</p>
-            </div>
-            <div className="flex flex-col items-center justify-center gap-1">
-              <p className="font-semibold text-lg text-gray-100">
-                {userDetails.age}
-              </p>
-              <p>Age</p>
-            </div>
-          </div>
-        </div>
-        <div className="mt-10">
-          <p className="text-black text-md font-bold">Goals</p>
-        </div> */}
+    <section className="h-full grid grid-auto-fit-lg w-full rounded-xl px-4 py-2 gap-4 border">
+      <div className="h-full p-2 gap-2">
+        <h1 className="text-orange-400 text-2xl font-bold tracking-tight">
+          <span className="text-foreground/30 text-sm mr-2">Pace</span>
+          Moderate
+        </h1>
+      </div>
+      <div className="h-full p-2 flex gap-2">
+        <h1 className="text-foreground text-2xl font-bold">
+          <span className="text-foreground/30 text-sm mr-2">
+            Starting Weight
+          </span>
+          185
+          <span className="inline-flex text-foreground/70 text-sm ml-1">
+            lb
+          </span>
+        </h1>
+      </div>
+      <div className="h-full p-2 flex text-center gap-2">
+        <h1 className="text-foreground text-2xl font-bold">
+          <span className="text-foreground/30 text-sm mr-2">Goal Weight</span>
+          170
+          <span className="inline-flex text-foreground/70 text-sm ml-1">
+            lb
+          </span>
+        </h1>
+      </div>
+      <div className="h-full p-2 flex text-center gap-2">
+        <h1 className="text-foreground text-2xl font-bold">
+          <span className="text-foreground/30 text-sm mr-2">
+            Current Weight
+          </span>
+          180
+          <span className="inline-flex text-foreground/70 text-sm ml-1">
+            lb
+          </span>
+          <span className="text-green-400 inline-flex text-xs gap-2 ml-2">
+            +12%
+            <Triangle size={12} className="rotate-180" />
+          </span>
+          {/* <span className="text-red-300 inline-flex text-xs gap-2">
+            12%
+            <Triangle size={12} />
+          </span> */}
+        </h1>
       </div>
     </section>
   );
