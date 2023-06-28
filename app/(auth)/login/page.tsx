e086b86d526846ea4553b1a65e000e491f743c03
rduraj96 <durajrei@gmail.com>
2023-06-28 03:06:52 -0400

    basic landing page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,19 +1,71 @@
 import Link from "next/link";
 import { LoginForm } from "./form";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Command } from "lucide-react";
+import Logo from "@/public/assets/5.svg";
+import Image from "next/image";
 
 export default function LoginPage() {
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
-      <div className="shadow-xl px-8 py-8 pt-12 space-y-12 bg-card border rounded-xl">
-        <h1 className="font-semibold text-2xl">Login</h1>
-        <LoginForm />
-        <p className="text-center">
-          Need to create an account?{" "}
-          <Link className="text-indigo-500 hover:underline" href="/register">
-            Create Account
-          </Link>
-        </p>
+    <>
+      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <Link
+          href="/register"
+          className={cn(
+            buttonVariants({ variant: "ghost" }),
+            "absolute right-4 top-4 md:right-8 md:top-8"
+          )}
+        >
+          Sign Up
+        </Link>
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+          <div className="absolute inset-0 bg-zinc-900" />
+          <div className="relative z-20 flex items-center text-lg font-bold gap-2">
+            <Image alt="logo" src={Logo} width={28} height={28} /> Maze Fit
+          </div>
+          <div className="relative z-20 mt-auto">
+            <blockquote className="space-y-2">
+              <p className="text-lg">
+                &ldquo;This library has saved me countless hours of work and
+                helped me deliver stunning designs to my clients faster than
+                ever before.&rdquo;
+              </p>
+              <footer className="text-sm">Sofia Davis</footer>
+            </blockquote>
+          </div>
+        </div>
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Existing User
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Enter your credentials below
+              </p>
+            </div>
+            <LoginForm />
+            <p className="px-8 text-center text-sm text-muted-foreground">
+              By clicking continue, you agree to our{" "}
+              <Link
+                href="/terms"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Terms of Service
+              </Link>{" "}
+              and{" "}
+              <Link
+                href="/privacy"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Privacy Policy
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
